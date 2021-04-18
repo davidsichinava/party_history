d2613cf3bcbbcfba66cea919d87201448f3b2630
David Sichinava <david@sichinava.ge>
2021-04-18 16:46:14 -0400

    update code and data

--- read.R
+++ read.R
@@ -16,47 +16,6 @@ pdata %>%
   mutate(party = factor(party, levels=c("Government", "Opposition", "No party", "DK", "RA"),
                         label = c("მთავრობა", "ოპოზიცია", "არცერთი", "არ ვიცი", "უარი პასუხზე")),
          party_in_government = factor(party_in_government, levels = c("unm", "gd"), labels=c("ენმ", "ქართული ოცნება")))%>%
-  # filter(party %in% c("მთავრობა", "ოპოზიცია"))%>%
-  ggplot()+
-  geom_point(aes(poll_date, value, group=factor(party), color=party_in_government), size=3, shape=19)+
-  # geom_point(aes(poll_date, value, group=factor(party), color=party_in_government), size=2)+
-  geom_smooth(aes(poll_date, value, method="loess"), span=0.3, se = FALSE)+
-  scale_color_manual(values=c("#d8171e", "#27509c"))+
-  annotate("rect", xmin=as.Date("2009-05-01"), xmax=as.Date("2012-10-01"), ymin=-Inf, ymax=Inf,
-           alpha=.1, fill="#d8171e", family = "FiraGO")+
-  annotate("rect", xmin=as.Date("2012-10-01"), xmax=as.Date("2021-02-01"), ymin=-Inf, ymax=Inf,
-           alpha=.1, fill="#27509c", family = "FiraGO")+
-  geom_vline(xintercept=as.Date(c("2012-10-01", "2016-10-8", "2020-10-31")),
-             color="grey", linetype = "longdash")+
-  annotate("text", x=as.Date("2012-10-01"), y=70, label="არჩევნები", family = "FiraGO", angle=90)+
-  annotate("text", x=as.Date("2016-10-08"), y=70, label="არჩევნები", family = "FiraGO", angle=90)+
-  annotate("text", x=as.Date("2020-10-31"), y=70, label="არჩევნები", family = "FiraGO", angle=90)+
-  guides(color=guide_legend(title="მმართველი პარტია"))+
-  facet_wrap(~party)+
-  ylim(0, 75)+
-  labs(
-    title = "შეხედულებებთან ახლოს მყოფი პოლიტიკური პარტია",
-    y = "%",
-    x = "გამოკითხვის თარიღი",
-    caption = "წყარო: NDI; CRRC-საქართველოს სხვადასხვა გამოკითხვა"
-  )+
-  theme_minimal()+
-  theme(
-    legend.title = element_blank(),
-    plot.title = element_text(family = "FiraGO", size=22),
-    strip.text = element_text(family = "FiraGO", size=18),
-    text = element_text(family= "FiraGO"),
-    legend.position = "none"
-  )
-
-pdata %>%
-  mutate(poll_date = as.Date(paste0(month, "/1/", year), "%m/%d/%Y"))%>%
-  group_by(poll_date, source)%>%
-  mutate(poll_id = cur_group_id())%>%
-  ungroup()%>%
-  mutate(party = factor(party, levels=c("Government", "Opposition", "No party", "DK", "RA"),
-                        label = c("მთავრობა", "ოპოზიცია", "არცერთი", "არ ვიცი", "უარი პასუხზე")),
-         party_in_government = factor(party_in_government, levels = c("unm", "gd"), labels=c("ენმ", "ქართული ოცნება")))%>%
   filter(party %in% c("მთავრობა", "ოპოზიცია"))%>%
   ggplot()+
   geom_point(aes(poll_date, value, color=party), size=4, shape=19, alpha=0.3)+
@@ -91,6 +50,48 @@ pdata %>%
   )
 
 ggsave("parties.png", height=8, width=13)
+
+pdata %>%
+  mutate(poll_date = as.Date(paste0(month, "/1/", year), "%m/%d/%Y"))%>%
+  group_by(poll_date, source)%>%
+  mutate(poll_id = cur_group_id())%>%
+  ungroup()%>%
+  mutate(party = factor(party, levels=c("Government", "Opposition", "No party", "DK", "RA")),
+         party_in_government = factor(party_in_government, levels = c("unm", "gd"), labels=c("UNM", "Georgian Dream")))%>%
+  filter(party %in% c("Government", "Opposition"))%>%
+  ggplot()+
+  geom_point(aes(poll_date, value, color=party), size=4, shape=19, alpha=0.3)+
+  # geom_point(aes(poll_date, value, group=factor(party), color=party), size=2, alpha=0.4)+
+  geom_smooth(aes(poll_date, value, method="loess", color=party), span=0.3, se = FALSE, size=2)+
+  scale_color_manual(values=c("#d8171e", "#27509c"))+
+  annotate("rect", xmin=as.Date("2009-05-01"), xmax=as.Date("2012-10-01"), ymin=-Inf, ymax=Inf,
+           alpha=.1, fill="#d8171e", family = "FiraGO")+
+  annotate("rect", xmin=as.Date("2012-10-01"), xmax=as.Date("2021-02-01"), ymin=-Inf, ymax=Inf,
+           alpha=.1, fill="#27509c", family = "FiraGO")+
+  geom_vline(xintercept=as.Date(c("2012-10-01", "2016-10-8", "2020-10-31")),
+             color="grey", linetype = "longdash")+
+  annotate("text", x=as.Date("2012-10-01"), y=70, label="Elections", family = "FiraGO", angle=90)+
+  annotate("text", x=as.Date("2016-10-08"), y=70, label="Elections", family = "FiraGO", angle=90)+
+  annotate("text", x=as.Date("2020-10-31"), y=70, label="Elections", family = "FiraGO", angle=90)+
+  guides(color=guide_legend(title="მმართველი პარტია"))+
+  # facet_wrap(~party)+
+  ylim(0, 75)+
+  labs(
+    title = "Party closest to the respondent's views",
+    y = "%",
+    x = "Poll date",
+    caption = "Source: NDI; CRRC-Georgia's different surveys. Trendline calculated using loess method"
+  )+
+  theme_minimal()+
+  theme(
+    legend.title = element_blank(),
+    plot.title = element_text(family = "FiraGO", size=22),
+    strip.text = element_text(family = "FiraGO", size=18),
+    text = element_text(family= "FiraGO"),
+    legend.position = "bottom"
+  )
+
+ggsave("parties_en.png", height=8, width=13)
 
 pdata %>%
   mutate(poll_date = as.Date(paste0(month, "/1/", year), "%m/%d/%Y"))%>%
@@ -134,6 +135,188 @@ pdata %>%
   )
 
 ggsave("dk_ra_na.png", height=8, width=13)
+
+
+pdata %>%
+  mutate(poll_date = as.Date(paste0(month, "/1/", year), "%m/%d/%Y"))%>%
+  group_by(poll_date, source)%>%
+  mutate(poll_id = cur_group_id())%>%
+  ungroup()%>%
+  mutate(party = factor(party, levels=c("Government", "Opposition", "No party", "DK", "RA")),
+         party_in_government = factor(party_in_government, levels = c("unm", "gd"), labels=c("UNM", "Georgian Dream")))%>%
+  filter(!party %in% c("Government", "Opposition"))%>%
+  ggplot()+
+  geom_point(aes(poll_date, value, color=party), size=4, shape=19, alpha=0.3)+
+  # geom_point(aes(poll_date, value, group=factor(party), color=party), size=2, alpha=0.4)+
+  geom_smooth(aes(poll_date, value, method="loess", color=party), span=0.3, se = FALSE, size=2)+
+  scale_color_manual(values=c("#e07a5f", "#3d405b", "#81b29a"))+
+  annotate("rect", xmin=as.Date("2009-05-01"), xmax=as.Date("2012-10-01"), ymin=-Inf, ymax=Inf,
+           alpha=.1, fill="#d8171e", family = "FiraGO")+
+  annotate("rect", xmin=as.Date("2012-10-01"), xmax=as.Date("2021-02-01"), ymin=-Inf, ymax=Inf,
+           alpha=.1, fill="#27509c", family = "FiraGO")+
+  geom_vline(xintercept=as.Date(c("2012-10-01", "2016-10-8", "2020-10-31")),
+             color="grey", linetype = "longdash")+
+  annotate("text", x=as.Date("2012-10-01"), y=70, label="Elections", family = "FiraGO", angle=90)+
+  annotate("text", x=as.Date("2016-10-08"), y=70, label="Elections", family = "FiraGO", angle=90)+
+  annotate("text", x=as.Date("2020-10-31"), y=70, label="Elections", family = "FiraGO", angle=90)+
+  guides(color=guide_legend(title="მმართველი პარტია"))+
+  # facet_wrap(~party)+
+  ylim(0, 75)+
+  labs(
+    title = "Party closest to the respondent's views",
+    y = "%",
+    x = "Poll date",
+    caption = "Source: NDI; CRRC-Georgia's different surveys. Trendline calculated using loess method"
+  )+
+  theme_minimal()+
+  theme(
+    legend.title = element_blank(),
+    plot.title = element_text(family = "FiraGO", size=22),
+    strip.text = element_text(family = "FiraGO", size=18),
+    text = element_text(family= "FiraGO"),
+    legend.position = "bottom"
+  )
+
+ggsave("dk_ra_na_en.png", height=8, width=13)
+
+
+pdata %>%
+  mutate(poll_date = as.Date(paste0(month, "/1/", year), "%m/%d/%Y"))%>%
+  group_by(poll_date, source)%>%
+  mutate(poll_id = cur_group_id())%>%
+  ungroup()%>%
+  mutate(party = factor(party, levels=c("Government", "Opposition", "No party", "DK", "RA")),
+         party_in_government = factor(party_in_government, levels = c("unm", "gd"), labels=c("UNM", "Georgian Dream")),
+         type = case_when(
+           party %in% c("Government", "Opposition") ~ 1,
+           T ~ 2
+         ),
+         type = factor(type, levels=c(1, 2), labels=c("პარტიული", "კუთვნილების გარეშე*"))
+         )%>%
+  # filter(party %in% c("Government", "Opposition"))%>%
+  group_by(poll_date, poll_id, type)%>%
+  summarize(value=sum(value))%>%
+  ggplot()+
+  geom_point(aes(poll_date, value, color=type), size=4, shape=19, alpha=0.3)+
+  geom_smooth(aes(poll_date, value, method="loess", color=type), span=0.3, se = FALSE, size=2)+
+  scale_color_manual(values=c("#1b9e77", "#d95f02"))+
+  annotate("rect", xmin=as.Date("2009-05-01"), xmax=as.Date("2012-10-01"), ymin=-Inf, ymax=Inf,
+           alpha=.1, fill="#d8171e", family = "FiraGO")+
+  annotate("rect", xmin=as.Date("2012-10-01"), xmax=as.Date("2021-02-01"), ymin=-Inf, ymax=Inf,
+           alpha=.1, fill="#27509c", family = "FiraGO")+
+  geom_vline(xintercept=as.Date(c("2012-10-01", "2016-10-8", "2020-10-31")),
+             color="grey", linetype = "longdash")+
+  annotate("text", x=as.Date("2012-10-01"), y=70, label="არჩევნები", family = "FiraGO", angle=90)+
+  annotate("text", x=as.Date("2016-10-08"), y=70, label="არჩევნები", family = "FiraGO", angle=90)+
+  annotate("text", x=as.Date("2020-10-31"), y=70, label="არჩევნები", family = "FiraGO", angle=90)+
+  guides(color=guide_legend(title="მმართველი პარტია"))+
+  ylim(0, 75)+
+  labs(
+    title = "შეხედულებებთან ახლოს მყოფი პოლიტიკური პარტია",
+    y = "%",
+    x = "გამოკითხვის თარიღი",
+    caption = "წყარო: NDI; CRRC-საქართველოს სხვადასხვა გამოკითხვა. ტენდენციის მრუდი გამოთვლილია ლოესის მეთოდით"
+  )+
+  theme_minimal()+
+  theme(
+    legend.title = element_blank(),
+    plot.title = element_text(family = "FiraGO", size=22),
+    strip.text = element_text(family = "FiraGO", size=18),
+    text = element_text(family= "FiraGO"),
+    legend.position = "bottom"
+  )
+
+ggsave("party.png", height=8, width=13)
+
+pdata %>%
+  mutate(poll_date = as.Date(paste0(month, "/1/", year), "%m/%d/%Y"))%>%
+  group_by(poll_date, source)%>%
+  mutate(poll_id = cur_group_id())%>%
+  ungroup()%>%
+  mutate(party = factor(party, levels=c("Government", "Opposition", "No party", "DK", "RA")),
+         party_in_government = factor(party_in_government, levels = c("unm", "gd"), labels=c("UNM", "Georgian Dream")),
+         type = case_when(
+           party %in% c("Government", "Opposition") ~ 1,
+           T ~ 2
+         ),
+         type = factor(type, levels=c(1, 2), labels=c("Partisan", "Unaffiliated*"))
+  )%>%
+  group_by(poll_id, poll_date, type)%>%
+  summarize(value=sum(value))%>%
+  ggplot()+
+  geom_point(aes(poll_date, value, color=type), size=4, shape=19, alpha=0.3)+
+  geom_smooth(aes(poll_date, value, method="loess", color=type), span=0.3, se = FALSE, size=2)+
+  scale_color_manual(values=c("#1b9e77", "#d95f02"))+
+  annotate("rect", xmin=as.Date("2009-05-01"), xmax=as.Date("2012-10-01"), ymin=-Inf, ymax=Inf,
+           alpha=.1, fill="#d8171e", family = "FiraGO")+
+  annotate("rect", xmin=as.Date("2012-10-01"), xmax=as.Date("2021-02-01"), ymin=-Inf, ymax=Inf,
+           alpha=.1, fill="#27509c", family = "FiraGO")+
+  geom_vline(xintercept=as.Date(c("2012-10-01", "2016-10-8", "2020-10-31")),
+             color="grey", linetype = "longdash")+
+  annotate("text", x=as.Date("2012-10-01"), y=70, label="Elections", family = "FiraGO", angle=90)+
+  annotate("text", x=as.Date("2016-10-08"), y=70, label="Elections", family = "FiraGO", angle=90)+
+  annotate("text", x=as.Date("2020-10-31"), y=70, label="Elections", family = "FiraGO", angle=90)+
+  guides(color=guide_legend(title="მმართველი პარტია"))+
+  ylim(0, 75)+
+  labs(
+    title = "Party closest to the respondent's views",
+    y = "%",
+    x = "Poll date",
+    caption = "Source: NDI; CRRC-Georgia's different surveys. Trendline calculated using loess method"
+  )+
+  theme_minimal()+
+  theme(
+    legend.title = element_blank(),
+    plot.title = element_text(family = "FiraGO", size=22),
+    strip.text = element_text(family = "FiraGO", size=18),
+    text = element_text(family= "FiraGO"),
+    legend.position = "bottom"
+  )
+
+ggsave("party_en.png", height=8, width=13)
+
+##### 
+
+pdata %>%
+  mutate(poll_date = as.Date(paste0(month, "/1/", year), "%m/%d/%Y"))%>%
+  group_by(poll_date, source)%>%
+  mutate(poll_id = cur_group_id())%>%
+  ungroup()%>%
+  mutate(party = factor(party, levels=c("Government", "Opposition", "No party", "DK", "RA"),
+                        label = c("მთავრობა", "ოპოზიცია", "არცერთი", "არ ვიცი", "უარი პასუხზე")),
+         party_in_government = factor(party_in_government, levels = c("unm", "gd"), labels=c("ენმ", "ქართული ოცნება")))%>%
+  # filter(party %in% c("მთავრობა", "ოპოზიცია"))%>%
+  ggplot()+
+  geom_point(aes(poll_date, value, group=factor(party), color=party_in_government), size=3, shape=19)+
+  # geom_point(aes(poll_date, value, group=factor(party), color=party_in_government), size=2)+
+  geom_smooth(aes(poll_date, value, method="loess"), span=0.3, se = FALSE)+
+  scale_color_manual(values=c("#d8171e", "#27509c"))+
+  annotate("rect", xmin=as.Date("2009-05-01"), xmax=as.Date("2012-10-01"), ymin=-Inf, ymax=Inf,
+           alpha=.1, fill="#d8171e", family = "FiraGO")+
+  annotate("rect", xmin=as.Date("2012-10-01"), xmax=as.Date("2021-02-01"), ymin=-Inf, ymax=Inf,
+           alpha=.1, fill="#27509c", family = "FiraGO")+
+  geom_vline(xintercept=as.Date(c("2012-10-01", "2016-10-8", "2020-10-31")),
+             color="grey", linetype = "longdash")+
+  annotate("text", x=as.Date("2012-10-01"), y=70, label="არჩევნები", family = "FiraGO", angle=90)+
+  annotate("text", x=as.Date("2016-10-08"), y=70, label="არჩევნები", family = "FiraGO", angle=90)+
+  annotate("text", x=as.Date("2020-10-31"), y=70, label="არჩევნები", family = "FiraGO", angle=90)+
+  guides(color=guide_legend(title="მმართველი პარტია"))+
+  facet_wrap(~party)+
+  ylim(0, 75)+
+  labs(
+    title = "შეხედულებებთან ახლოს მყოფი პოლიტიკური პარტია",
+    y = "%",
+    x = "გამოკითხვის თარიღი",
+    caption = "წყარო: NDI; CRRC-საქართველოს სხვადასხვა გამოკითხვა"
+  )+
+  theme_minimal()+
+  theme(
+    legend.title = element_blank(),
+    plot.title = element_text(family = "FiraGO", size=22),
+    strip.text = element_text(family = "FiraGO", size=18),
+    text = element_text(family= "FiraGO"),
+    legend.position = "none"
+  )
 
 pdata %>%
   mutate(poll_date = as.Date(paste0(month, "/1/", year), "%m/%d/%Y"))%>%
